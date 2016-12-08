7ea93ff73dd4cdf261cfdac05905ce77c8a916c9
Bogdan Popov <bogdan.popov@mail.ru>
2016-12-09 00:06:10 +0300

    GraphBuilder updated

--- hexlet/multi/Game/src/main/java/ru/popov/bodya/machineintelligence/GraphBuilder.java
+++ hexlet/multi/Game/src/main/java/ru/popov/bodya/machineintelligence/GraphBuilder.java
@@ -3,24 +3,48 @@ package ru.popov.bodya.machineintelligence;
 import ru.popov.bodya.pojo.GraphNode;
 import ru.popov.bodya.pojo.XOField;
 
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
+import java.util.concurrent.Callable;
+import java.util.concurrent.CopyOnWriteArraySet;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Future;
 
-public class GraphBuilder {
+public class GraphBuilder implements Callable<GraphNode> {
 
-    public GraphNode build(final XOField.Figure currentFigure, final XOField currentField) {
-        final XOField.Figure nextFigure = currentFigure == XOField.Figure.O ? XOField.Figure.X : XOField.Figure.O;
-        final Set<GraphNode> children = new HashSet<>();
+    private final ExecutorService executorService;
+
+    private final XOField.Figure nextFigure;
+
+    private final XOField currentFiled;
+
+    private final int deepLevel;
+
+    public GraphBuilder(ExecutorService executorService, XOField.Figure nextFigure, XOField currentFiled, int deepLevel) {
+        this.executorService = executorService;
+        this.nextFigure = nextFigure;
+        this.currentFiled = currentFiled;
+        this.deepLevel = deepLevel;
+    }
+
+    @Override
+    public GraphNode call() throws Exception {
+        final List<Future<GraphNode>> futures = new ArrayList<>();
+        final Set<GraphNode> children = new CopyOnWriteArraySet<>();
         for (int y = 0; y < 3; y++) {
             for (int x = 0; x < 3; x++) {
-                if (currentField.getFigure(x, y) == null) {
-                    final XOField newField = new XOField(currentField);
-                    newField.setFigure(x, y, nextFigure);
-                    children.add(build(nextFigure, newField));
+                if (currentFiled.getFigure(x, y) != null) {
+                    continue;
                 }
-            }
+                final XOField newField = new XOField(currentFiled);
+                newField.setFigure(x, y, nextFigure);
+                final GraphBuilder graphBuilder = new GraphBuilder(executorService, nextFigure, newField, deepLevel + 1);
 
+            }
         }
-        return new GraphNode(currentField, children);
     }
+
+
 }
