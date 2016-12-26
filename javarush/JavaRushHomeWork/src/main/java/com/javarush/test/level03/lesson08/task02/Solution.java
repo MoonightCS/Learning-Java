2c6bc239bd297d8709800e742fe6440e3eb3185f
Bogdan Popov <bogdan.popov@mail.ru>
2016-12-26 14:52:26 +0300

    some solutions added

---
+++ javarush/JavaRushHomeWork/src/main/java/com/javarush/test/level03/lesson08/task02/Solution.java
@@ -0,0 +1,21 @@
+package com.javarush.test.level03.lesson08.task02;
+
+/* Зарплата через 5 лет
+Ввести с клавиатуры отдельно Имя, число1, число2. Вывести надпись:
+«Имя» получает «число1» через «число2» лет.
+Пример: Коля получает 3000 через 5 лет.
+*/
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String name = reader.readLine().trim();
+        int number1 = Integer.parseInt(reader.readLine().trim());
+        int number2 = Integer.parseInt(reader.readLine().trim());
+        System.out.printf("%s получает %d через %d лет.", name, number1, number2);
+
+    }
+}
