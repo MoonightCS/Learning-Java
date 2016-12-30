2158d270d7c663d6bd98cd3cb536a17df812e1bb
Moonlight <bogdan.popov@mail.ru>
2016-12-30 19:50:58 +0300

    some soltuions added

---
+++ javarush/JavaRushHomeWork/src/main/java/com/javarush/test/level04/lesson10/task04/Solution.java
@@ -0,0 +1,24 @@
+package com.javarush.test.level04.lesson10.task04;
+
+import java.io.*;
+
+/* S-квадрат
+Вывести на экран квадрат из 10х10 букв S используя цикл while.
+Буквы в каждой строке не разделять.
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        int x = 10;
+        while (x > 0) {
+            int y = 10;
+            while (y > 0) {
+                System.out.print("S");
+                y--;
+            }
+            System.out.println();
+            x--;
+        }
+
+    }
+}
