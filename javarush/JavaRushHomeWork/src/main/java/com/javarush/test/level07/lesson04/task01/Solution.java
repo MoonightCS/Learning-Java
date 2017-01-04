83b6c5de6444b1bd5d2cf8bb202b8856df7426f2
Moonlight <bogdan.popov@mail.ru>
2017-01-04 14:31:50 +0300

    some solutions added

--- javarush/JavaRushHomeWork/src/main/java/com/javarush/test/level07/lesson04/task01/Solution.java
+++ javarush/JavaRushHomeWork/src/main/java/com/javarush/test/level07/lesson04/task01/Solution.java
@@ -3,6 +3,9 @@ package com.javarush.test.level07.lesson04.task01;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
 
 /* Максимальное среди массива на 20 чисел
 1. В методе initializeArray():
@@ -20,15 +23,20 @@ public class Solution
         System.out.println(max);
     }
     public static int[] initializeArray() throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         int[] arr = new int[20];
         for (int i = 0; i < 20; i++) {
-
+            arr[i] = Integer.parseInt(reader.readLine());
         }
         return arr;
     }
 
     public static int max(int[] array) {
-        //Найдите максимальное значение в этом методе
-        return 0;
+        int max = Integer.MIN_VALUE;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] > max)
+                max = array[i];
+        }
+        return max;
     }
 }
