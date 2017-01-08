da49ffb989a9c7cf34a25b95dc8bdfded3df8d50
Moonlight <bogdan.popov@mail.ru>
2017-01-09 02:31:13 +0300

    some solutions added

---
+++ javarush/JavaRushHomeWork/src/main/java/ru/popov/bodya/date/DaysUntilNewYear.java
@@ -0,0 +1,27 @@
+package ru.popov.bodya.date;
+
+import java.util.Calendar;
+import java.util.Date;
+
+public class DaysUntilNewYear {
+    public static void main(String[] args) {
+
+        Calendar cal = Calendar.getInstance();
+        cal.set(Calendar.HOUR, 0);
+        cal.set(Calendar.MINUTE, 0);
+        cal.set(Calendar.SECOND, 0);
+        cal.set(Calendar.DATE, 1);
+        cal.set(Calendar.MONTH, 0);
+
+        Date date = cal.getTime();
+
+
+        Date currentTime = new Date();
+
+        long msTimeDistance = currentTime.getTime() - date.getTime();
+        long msDay = 24 * 60 * 60 * 1000;  //сколько миллисекунд в одних сутках
+
+        int dayCount = (int) (msTimeDistance/msDay); //количество целых дней
+        System.out.println("Days from start of year: " + dayCount);
+    }
+}
