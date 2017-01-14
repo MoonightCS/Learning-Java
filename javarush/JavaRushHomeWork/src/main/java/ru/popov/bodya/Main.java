6a5da953fa88115fe62f130a1509ffc0ecd4b853
Moonlight <bogdan.popov@mail.ru>
2017-01-14 03:45:05 +0300

    some solutions from 10lvl added

--- javarush/JavaRushHomeWork/src/main/java/ru/popov/bodya/Main.java
+++ javarush/JavaRushHomeWork/src/main/java/ru/popov/bodya/Main.java
@@ -2,6 +2,15 @@ package ru.popov.bodya;
 
 public class Main {
     public static void main(String[] args) {
+        int a = 0;
+        int b = a + 46;
+        byte c = (byte) (a * b);
+        double f = (char) 1234.15;
+        char ch = (char) 1234.15;
+
+        System.out.println((char) 1234.15);
+        long d = (long) (a + f / c + b);
+        System.out.println(d);
     }
 
 }
