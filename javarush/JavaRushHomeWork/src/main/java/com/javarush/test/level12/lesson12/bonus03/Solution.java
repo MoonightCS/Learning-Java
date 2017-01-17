2b9235a8381c10e713d1064938fe5f96764e34b6
Bogdan Popov <bogdan.popov@mail.ru>
2017-01-17 13:14:49 +0300

    some 12 lvl solutions added

---
+++ javarush/JavaRushHomeWork/src/main/java/com/javarush/test/level12/lesson12/bonus03/Solution.java
@@ -0,0 +1,43 @@
+package com.javarush.test.level12.lesson12.bonus03;
+
+/* Задача по алгоритмам
+Написать метод, который возвращает минимальное число в массиве и его позицию (индекс).
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        int[] data = new int[]{1, 2, 3, 5, -2, -8, 0, 77, 5, 5};
+
+        Pair<Integer, Integer> result = getMinimumAndIndex(data);
+
+        System.out.println("Minimum is " + result.x);
+        System.out.println("Index of minimum element is " + result.y);
+    }
+
+    public static Pair<Integer, Integer> getMinimumAndIndex(int[] array) {
+        if (array == null || array.length == 0) {
+            return new Pair<>(null, null);
+        }
+        Pair<Integer, Integer> pair = new Pair<>(0, 0);
+        int min = Integer.MAX_VALUE;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] < min) {
+                min = array[i];
+                pair.x = min;
+                pair.y = i;
+            }
+        }
+        return pair;
+    }
+
+
+    public static class Pair<X, Y> {
+        public X x;
+        public Y y;
+
+        public Pair(X x, Y y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
+}
