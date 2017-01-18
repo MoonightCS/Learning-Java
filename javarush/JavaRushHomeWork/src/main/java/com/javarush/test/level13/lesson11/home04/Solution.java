f912640e3415145fc27ef684af8091bf671dc71b
Bogdan Popov <bogdan.popov@mail.ru>
2017-01-18 16:01:28 +0300

    some 13 lvl solutions added

---
+++ javarush/JavaRushHomeWork/src/main/java/com/javarush/test/level13/lesson11/home04/Solution.java
@@ -0,0 +1,32 @@
+package com.javarush.test.level13.lesson11.home04;
+
+/* Запись в файл
+1. Прочесть с консоли имя файла.
+2. Считывать строки с консоли, пока пользователь не введет строку "exit".
+3. Вывести абсолютно все введенные строки в файл, каждую строчку с новой стороки.
+*/
+
+import java.io.BufferedReader;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Solution {
+    public static void main(String[] args) {
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+             FileOutputStream fileOutputStream = new FileOutputStream(reader.readLine().trim())) {
+            while (true) {
+                String string = reader.readLine();
+                if (string.equals("exit")) {
+                    string += "\n";
+                    fileOutputStream.write(string.getBytes());
+                    break;
+                }
+                string += "\n";
+                fileOutputStream.write(string.getBytes());
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
