b1958b6bdc2c5e9727a973faa2e509e073b08177
Bogdan Popov <bogdan.popov@mail.ru>
2017-01-19 15:52:52 +0300

    some 14 lvl solutions added

--- javarush/JavaRushHomeWork/src/main/java/com/javarush/test/level14/lesson08/home05/Mouse.java
+++ javarush/JavaRushHomeWork/src/main/java/com/javarush/test/level14/lesson08/home05/Mouse.java
@@ -1,6 +1,6 @@
 package com.javarush.test.level14.lesson08.home05;
 
-public class Mouse implements CompItem{
+public class Mouse implements CompItem {
     @Override
     public String getName() {
         return "Mouse";
