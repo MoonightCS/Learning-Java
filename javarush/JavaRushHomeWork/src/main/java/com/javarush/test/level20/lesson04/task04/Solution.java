f2f6245e01e8ab8b0e18d930fec5995637dd0372
Bogdan Popov <bogdan.popov@mail.ru>
2017-02-02 17:09:58 +0300

    20 lvl solutions added

--- javarush/JavaRushHomeWork/src/main/java/com/javarush/test/level20/lesson04/task04/Solution.java
+++ javarush/JavaRushHomeWork/src/main/java/com/javarush/test/level20/lesson04/task04/Solution.java
@@ -1,52 +1,50 @@
 package com.javarush.test.level20.lesson04.task04;
 
-import java.io.Serializable;
+import java.io.*;
 
 /* Как сериализовать static?
 Сделайте так, чтобы сериализация класса ClassWithStatic была возможной
 */
 public class Solution {
-    /*public static void main(String[] args) throws IOException, ClassNotFoundException {
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
 
-        String fileName  = "/Users/bogdanpopov/Desktop/learning-java/javarush/JavaRushHomeWork/src/test/resources/file1.txt";
+        String fileName = "/Users/bogdanpopov/Desktop/learning-java/javarush/JavaRushHomeWork/src/test/resources/file1.txt";
         ObjectOutputStream objectOutputStream = new ObjectOutputStream(new FileOutputStream(fileName));
         ClassWithStatic classWithStatic = new ClassWithStatic();
         classWithStatic.i = 1;
         classWithStatic.j = 9;
-        classWithStatic.writeExternal(objectOutputStream);
+        objectOutputStream.writeObject(classWithStatic);
         objectOutputStream.close();
 
         ObjectInputStream objectInputStream = new ObjectInputStream(new FileInputStream(fileName));
-        ClassWithStatic classWithStatic1 = new ClassWithStatic();
-        classWithStatic1.readExternal(objectInputStream);
+        ClassWithStatic classWithStatic1 = (ClassWithStatic) objectInputStream.readObject();
         objectInputStream.close();
 
         System.out.println(classWithStatic.i);
         System.out.println(classWithStatic.j);
         System.out.println(ClassWithStatic.staticString);
 
-        System.out.println("______________");
+        System.out.println("______________\n");
         System.out.println(classWithStatic1.i);
         System.out.println(classWithStatic1.j);
         System.out.println(ClassWithStatic.staticString);
-    }*/
-    public static class ClassWithStatic implements Serializable {
+    }
+
+    public static class ClassWithStatic implements Externalizable {
         public static String staticString = "it's test static string";
         public int i;
         public int j;
 
-        /*@Override
+        @Override
         public void writeExternal(ObjectOutput out) throws IOException {
-            out.writeObject(staticString);
-            out.writeInt(i);
-            out.writeInt(j);
+            out.writeInt(this.i);
+            out.writeInt(this.j);
         }
 
         @Override
         public void readExternal(ObjectInput in) throws IOException, ClassNotFoundException {
-            staticString = (String)in.readObject();
-            i = in.readInt();
-            j = in.readInt();
-        }*/
+            this.i = in.readInt();
+            this.j = in.readInt();
+        }
     }
 }
---
+++ javarush/JavaRushHomeWork/src/main/java/com/javarush/test/level20/lesson10/home07/Solution.java
@@ -0,0 +1,61 @@
+package com.javarush.test.level20.lesson10.home07;
+
+import java.io.*;
+
+/* Переопределение сериализации в потоке
+Сериализация/десериализация Solution не работает.
+Исправьте ошибки не меняя сигнатуры методов и класса.
+Метод main не участвует в тестировании.
+Написать код проверки самостоятельно в методе main:
+1) создать экземпляр класса Solution
+2) записать в него данные  - writeObject
+3) сериализовать класс Solution  - writeObject(ObjectOutputStream out)
+4) десериализовать, получаем новый объект
+5) записать в новый объект данные - writeObject
+6) проверить, что в файле есть данные из п.2 и п.5
+*/
+public class Solution implements Serializable, AutoCloseable {
+    private transient FileOutputStream stream;
+    private String fileName;
+
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
+        Solution solution = new Solution("src/test/resources/file1.txt");
+        solution.writeObject("hello my dear friends");
+        ObjectOutputStream stream = new ObjectOutputStream(new FileOutputStream("src/test/resources/file2.txt"));
+        stream.writeObject(solution);
+        stream.close();
+
+        ObjectInputStream objectInputStream = new ObjectInputStream(new FileInputStream("src/test/resources/file2.txt"));
+        Solution solution1;
+        solution1 = (Solution) objectInputStream.readObject();
+        solution1.writeObject("yoyo");
+        objectInputStream.close();
+
+    }
+
+    public Solution(String fileName) throws FileNotFoundException {
+        this.fileName = fileName;
+        this.stream = new FileOutputStream(fileName);
+    }
+
+    public void writeObject(String string) throws IOException {
+        stream.write(string.getBytes());
+        stream.write("\n".getBytes());
+        stream.flush();
+    }
+
+    private void writeObject(ObjectOutputStream out) throws IOException {
+        out.defaultWriteObject();
+    }
+
+    private void readObject(ObjectInputStream in) throws IOException, ClassNotFoundException {
+        in.defaultReadObject();
+        stream = new FileOutputStream(this.fileName, true);
+    }
+
+    @Override
+    public void close() throws Exception {
+        System.out.println("Closing everything!");
+        stream.close();
+    }
+}
