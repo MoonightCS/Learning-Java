71b9f1341735f31616851e0a92957dd81cc6a8bc
Bogdan Popov <bogdan.popov@mail.ru>
2017-02-07 20:33:34 +0300

    java rush 2.0 added

---
+++ javarush/JavaRush2.0/JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task22/task2210/Solution.java
@@ -0,0 +1,21 @@
+package com.javarush.task.task22.task2210;
+
+/* 
+Используя StringTokenizer разделить query на части по разделителю delimiter.
+
+Пример
+getTokens("level22.lesson13.task01", ".")
+
+Возвращает
+{"level22", "lesson13", "task01"}
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    public static String[] getTokens(String query, String delimiter) {
+        return null;
+    }
+}
