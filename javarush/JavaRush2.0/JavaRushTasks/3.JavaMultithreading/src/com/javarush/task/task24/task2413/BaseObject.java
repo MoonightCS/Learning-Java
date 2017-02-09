c44951c886c4a27c33491fdf684d1d3c78b5e800
Bogdan Popov <popovbodya@gmail.com>
2017-02-09 18:26:39 +0300

    some 24 lvl solutions added

--- javarush/JavaRush2.0/JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task24/task2413/BaseObject.java
+++ javarush/JavaRush2.0/JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task24/task2413/BaseObject.java
@@ -1,45 +1,16 @@
 package com.javarush.task.task24.task2413;
 
-/*
-Арканоид(8)
-Но и это еще не все.
-Классу BaseObject нужны еще методы.
-Пока это будут пустые методы draw(Canvas canvas) и move().
-Классы-наследники должны будут переопределить их у себя и реализовать необходимую функциональность (пока можешь оставить их пустыми, просто чтобы код компилировался).
-Объяви эти методы, но сделай их абстрактными.
-Также сделай абстрактным сам класс BaseObject.
-
-А еще нам нужно будет определять попал шарик в кирпич или в подставку.
-Это будем делать так:
-В этом же классе, создадим специальный метод: boolean isIntersec(BaseObject o)
-Он будет определять — «пересеклись» объекты или нет. Если пересеклись — возвращать true, если нет — false.
-
-Т.к. объекты мы условно считаем кругами, то предлагаю такую формулу взаимодействия
-Если центр круга одного объекта попал в круг другого, то будем считать, что они столкнулись.
-
-Или еще проще:
-дистанция_между_объектами <= max (радиус_первого_объекта, радиус_второго_объекта)
+/**
+ * Базовый класс для всех объектов игры.
  */
-
 public abstract class BaseObject {
+    //координаты
+    protected double x;
+    protected double y;
+    //радиус объекта
+    protected double radius;
 
-    private double x;
-    private double y;
-    private double radius;
-
-    public boolean isIntersec(BaseObject o) {
-        double dx = x - o.x;
-        double dy = y - o.y;
-        double realDest = Math.sqrt(dx * dx + dy * dy);
-        double dest = Math.max(radius, o.radius);
-        return realDest <= dest;
-    }
-
-    public abstract void draw(Canvas canvas);
-
-    public abstract void move();
-
-    public BaseObject(double x, double y, double radius) {
+    protected BaseObject(double x, double y, double radius) {
         this.x = x;
         this.y = y;
         this.radius = radius;
@@ -49,29 +20,54 @@ public abstract class BaseObject {
         return x;
     }
 
-    public double getY() {
-        return y;
-    }
-
-    public double getRadius() {
-        return radius;
-    }
-
     public void setX(double x) {
         this.x = x;
+    }
+
+    public double getY() {
+        return y;
     }
 
     public void setY(double y) {
         this.y = y;
     }
 
+    public double getRadius() {
+        return radius;
+    }
+
     public void setRadius(double radius) {
         this.radius = radius;
     }
 
-//    private double findDistance(BaseObject o1, BaseObject o2) {
-//        double xDif = o2.getX() - o1.getX();
-//        double yDif = o2.getY() - o2.getY();
-//        return Math.sqrt(xDif * xDif + yDif * yDif);
-//    }
+    /**
+     * Метод рисует свой объект на "канвасе".
+     */
+    abstract void draw(Canvas canvas);
+
+    /**
+     * Двигаем себя на один ход.
+     */
+    abstract void move();
+
+    /**
+     * Проверяем - не выходит ли (x,y) за границы.
+     */
+    void checkBorders(double minx, double maxx, double miny, double maxy) {
+        if (x < minx) x = minx;
+        if (x > maxx) x = maxx;
+        if (y < miny) y = miny;
+        if (y > maxy) y = maxy;
+    }
+
+    /**
+     * Проверяем - пересекаются ли переданный(o) и наш(this) объекты.
+     */
+    boolean isIntersec(BaseObject o) {
+        double dx = x - o.x;
+        double dy = y - o.y;
+        double destination = Math.sqrt(dx * dx + dy * dy);
+        double destination2 = Math.max(radius, o.radius);
+        return destination <= destination2;
+    }
 }
--- javarush/JavaRush2.0/JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task24/task2413/Canvas.java
+++ javarush/JavaRush2.0/JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task24/task2413/Canvas.java
@@ -1,30 +1,75 @@
 package com.javarush.task.task24.task2413;
 
-/*
-Арканоид(9)
-Теперь займемся классом Canvas.
-Он у нас будет содержать матрицу (двумерный массив), куда мы будем «рисовать«.
-У матрицы есть ширина и высота.
-А еще будем в ней хранить не числа (int), а символы (char).
-
-Надо:
-а) Добавить в класс два приватных поля width и height.
-б) Добавить в класс приватное поле matrix (char[][]).
-в) Добавить конструктор с двумя параметрами типа int (width и height), поле matrix должно быть инициализировано пустым массивом размерностью [height+2][width+2].
-г) Добавить геттеры и сеттеры для всех приватных полей класса.
-
-
+/**
+ * Класс-холст для отрисовки.
  */
 public class Canvas {
-
+    //ширина и высота
     private int width;
     private int height;
+    //матрица, где рисуем. символ - это цвет.
     private char[][] matrix;
 
     public Canvas(int width, int height) {
         this.width = width;
         this.height = height;
-        matrix = new char[this.height + 2][this.width + 2];
+        this.matrix = new char[height + 2][width + 2];
+    }
+
+    /**
+     * Очищаем холст
+     */
+    void clear() {
+        this.matrix = new char[height + 2][width + 2];
+    }
+
+    /**
+     * Печатаем переданную фигуру в указанных координатах цветом c.
+     * Если переданный массив содержит единицы, то на холсте им будут соответствовать символы - с.
+     */
+    void drawMatrix(double x, double y, int[][] matrix, char c) {
+        int height = matrix.length;
+        int width = matrix[0].length;
+
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < width; j++) {
+                if (matrix[i][j] == 1)
+                    setPoint(x + j, y + i, c);
+            }
+        }
+    }
+
+    /**
+     * Ставим одну точку на холсте с координатами (x,y) и цветом - c.
+     */
+    void setPoint(double x, double y, char c) {
+        int x0 = (int) Math.round(x);
+        int y0 = (int) Math.round(y);
+        if (y0 < 0 || y0 >= matrix.length) return;
+        if (x0 < 0 || x0 >= matrix[y0].length) return;
+
+        matrix[y0][x0] = c;
+    }
+
+    /**
+     * Печатаем содержимое холста на экран.
+     */
+    void print() {
+        System.out.println();
+
+        for (int i = 0; i < height + 2; i++) {
+            for (int j = 0; j < width + 2; j++) {
+                System.out.print(" ");
+                System.out.print(matrix[i][j]);
+                System.out.print(" ");
+            }
+
+            System.out.println();
+        }
+
+        System.out.println();
+        System.out.println();
+        System.out.println();
     }
 
     public int getWidth() {
@@ -37,17 +82,5 @@ public class Canvas {
 
     public char[][] getMatrix() {
         return matrix;
-    }
-
-    public void setWidth(int width) {
-        this.width = width;
-    }
-
-    public void setHeight(int height) {
-        this.height = height;
-    }
-
-    public void setMatrix(char[][] matrix) {
-        this.matrix = matrix;
     }
 }
