b90ef39546b633b0478e5f6e15b657caa8bcf720
Bogdan Popov <popovbodya@gmail.com>
2017-02-14 14:54:37 +0300

    some 25 lvl solutions added

---
+++ javarush/JavaRush2.0/JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task25/task2503/Solution.java
@@ -0,0 +1,37 @@
+package com.javarush.task.task25.task2503;
+
+/* 
+Свой enum
+Реализуй интерфейс Columnable у Column, описание методов смотрите в джавадоках.
+Реализуй логику метода Column.getVisibleColumns.
+Метод Column.configureColumns уже реализован, его не меняй.
+Создавать дополнительные поля нельзя.
+Метод main не участвует в тестировании.
+*/
+public class Solution {
+    /**
+     * Output:
+     * <p/>
+     * Available Amount
+     * Account Number
+     * Bank Name
+     * --------------------
+     * Available Amount
+     * Bank Name
+     */
+    public static void main(String[] args) {
+
+        Column.configureColumns(Column.Amount, Column.AccountNumber, Column.BankName);
+
+        for (Columnable columnable : Column.getVisibleColumns()) {
+            System.out.println(columnable.getColumnName());
+        }
+
+        System.out.println("--------------------");
+        Column.AccountNumber.hide();
+
+        for (Columnable columnable : Column.getVisibleColumns()) {
+            System.out.println(columnable.getColumnName());
+        }
+    }
+}
