9fdad2e36a01b6771e32a3d3587b95078ae53e32
Bogdan Popov <popovbodya@gmail.com>
2017-02-22 19:47:24 +0300

    some solutions of big task added

--- javarush/JavaRush2.0/JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task29/task2909/user/User.java
+++ javarush/JavaRush2.0/JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task29/task2909/user/User.java
@@ -1,20 +1,24 @@
 package com.javarush.task.task29.task2909.user;
 
+/*
+В классе User необходимо переписать методы: getAddress, getCountry, setCountry, getCity, setCity.
+И нужно добавить в класс приватное поле Address address.
+
+
+ */
 public class User {
     private String name;
     private String surname;
     private int age;
-
-    private String country;
-    private String city;
-    private House house;
-
+    private boolean man;
+    private Address address;
     private Work work;
 
     public User(String name, String surname, int age) {
         this.name = name;
         this.surname = surname;
         this.age = age;
+        this.address = new Address();
     }
 
     public String getName() {
@@ -42,24 +46,26 @@ public class User {
     }
 
     public String getCountry() {
-        return country;
+        return address.getCountry();
     }
 
     public void setCountry(String country) {
-        this.country = country;
+        address.setCountry(country);
     }
 
     public String getCity() {
-        return city;
+        return address.getCity();
     }
 
+
     public void setCity(String city) {
-        this.city = city;
+        address.setCity(city);
     }
 
     public String getAddress() {
-        return country + " " + city + " " + house.house;
+        return address.getCountry() + " " + address.getCity() + " " + address.getHouse();
     }
+
 
     public Work getWork() {
         return work;
@@ -67,5 +73,31 @@ public class User {
 
     public void setWork(Work work) {
         this.work = work;
+    }
+
+    public boolean isMan() {
+        return man;
+    }
+
+    public void setMan(boolean man) {
+        this.man = man;
+    }
+
+    public void printInfo() {
+        System.out.println("Имя: " + getName());
+        System.out.println("Фамилия: " + getSurname());
+        printAdditionalInfo();
+    }
+
+    public void printAdditionalInfo() {
+        if (getAge() < 16)
+            System.out.println("Пользователь моложе 16 лет");
+        else
+            System.out.println("Пользователь старше 16 лет");
+    }
+
+    public String getBoss() {
+        Work work = getWork();
+        return work.getBoss();
     }
 }
--- javarush/JavaRush2.0/JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task29/task2909/user/UserHelper.java
+++ javarush/JavaRush2.0/JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task29/task2909/user/UserHelper.java
@@ -2,54 +2,58 @@ package com.javarush.task.task29.task2909.user;
 
 import java.util.concurrent.atomic.AtomicInteger;
 
+/*
+Рефакторинг (14)
+14.1. Перемещение поля. Замени поля isManAnya и isManRoma полем man в нужном классе.
+Добавь сеттер и геттер для нового поля (при выборе имен методов учти тип поля).
+14.2. Извлечение класса.
+14.2.1. Добавь класс Address в пакет user.
+14.2.2. Перенеси поля country, city и house в новый класс.
+14.2.3. Добавь сеттеры и геттеры для них.
+14.2.4. Перепиши класс User, используя поле класса Address address.
+14.3. Встраивание класса. Класс House почти ничего не делает, избавься от него.
+14.4. Сокрытие делегирования.
+14.4.1. Добавь в класс User метод getBoss().
+14.4.2. Перепиши реализацию метода getBossName(User user) класса UserHelper.
+ */
+
 public class UserHelper {
     private User userAnya = new User("Аня", "Смирнова", 10);
     private User userRoma = new User("Рома", "Виноградов", 30);
 
-    private boolean isManAnya = false;
-    private boolean isManRoma = true;
+    {
+        userAnya.setMan(false);
+        userRoma.setMan(true);
+    }
 
     public void printUsers() {
-        System.out.println("Имя: " + userAnya.getName());
-        System.out.println("Фамилия: " + userAnya.getSurname());
-        printAdditionalInfo(userAnya);
-
-        System.out.println("Имя: " + userRoma.getName());
-        System.out.println("Фамилия: " + userRoma.getSurname());
-        printAdditionalInfo(userRoma);
-    }
-
-    public void printAdditionalInfo(User user) {
-        if (ageLessThan16(user))
-            System.out.println("Пользователь моложе 16 лет");
-        else
-            System.out.println("Пользователь старше 16 лет");
-    }
-
-    private boolean ageLessThan16(User user) {
-        if (user.getAge() < 16) {
-            return true;
-        }
-        return false;
+        userAnya.printInfo();
+        userAnya.printAdditionalInfo();
+        userRoma.printInfo();
+        userRoma.printAdditionalInfo();
     }
 
     public int calculateAverageAge() {
-        int age = 28;
-        User userUra = new User("Юра", "Карп", age);
-
-        age = (userAnya.getAge() + userRoma.getAge() + userUra.getAge()) / 3;
-
-        return age;
+        User userUra = new User("Юра", "Карп", 28);
+        return (userAnya.getAge() + userRoma.getAge() + userUra.getAge()) / 3;
     }
 
-    public void calculateRate(AtomicInteger base, int age, boolean hasWork, boolean hasHouse) {
-        base.set(base.get() + age / 100);
-        base.set((int) (base.get() * (hasWork ? 1.1 : 0.9)));
-        base.set((int) (base.get() * (hasHouse ? 1.1 : 0.9)));
+    public int calculateRate(AtomicInteger base, int age, boolean hasWork, boolean hasHouse) {
+
+        int result = base.intValue();
+        result += age / 100;
+        result *= hasWork ? 1.1 : 0.9;
+        result *= hasHouse ? 1.1 : 0.9;
+        return result;
     }
+
+    /*
+    Необходимо изменить реализацию метода getBossName(User user) класса UserHelper (используй метод getBoss() класса User).
+     */
 
     public String getBossName(User user) {
-        Work work = user.getWork();
-        return work.getBoss();
+        return user.getBoss();
     }
+
+
 }
