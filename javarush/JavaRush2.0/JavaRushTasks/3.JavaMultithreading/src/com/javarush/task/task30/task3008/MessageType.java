23835436e4df4f15dea2750462cbc95429561565
Bogdan Popov <popovbodya@gmail.com>
2017-03-02 06:36:35 +0300

    some solutions of big task added

---
+++ javarush/JavaRush2.0/JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task30/task3008/MessageType.java
@@ -0,0 +1,22 @@
+package com.javarush.task.task30.task3008;
+
+/*
+Добавь для каждого пункта вышеописанного протокола соответствующее значение в enum
+MessageType:
+1) NAME_REQUEST – запрос имени.
+2) USER_NAME – имя пользователя.
+3) NAME_ACCEPTED – имя принято.
+4) TEXT – текстовое сообщение.
+5) USER_ADDED – пользователь добавлен.
+6) USER_REMOVED – пользователь удален.
+ */
+
+public enum MessageType {
+
+    NAME_REQUEST,
+    USER_NAME,
+    NAME_ACCEPTED,
+    TEXT,
+    USER_ADDED,
+    USER_REMOVED
+}
