40ee534426913c69be0b22af46d8ca75cacfa828
Bogdan Popov <popovbodya@gmail.com>
2017-03-05 16:08:55 +0300

    some 27 lvl solutions added

---
+++ javarush/JavaRush2.0/JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task27/task2711/Solution.java
@@ -0,0 +1,32 @@
+package com.javarush.task.task27.task2711;
+
+import java.util.concurrent.CountDownLatch;
+
+/* 
+CountDownLatch
+Дана стандартная реализация методологии wait-notify.
+Почитай про CountDownLatch и перепиши тело метода someMethod используя поле latch.
+Весь лишний код удали из класса.
+*/
+public class Solution {
+
+    private volatile boolean isWaitingForValue = true;
+
+    CountDownLatch latch = new CountDownLatch(1);
+
+    public void someMethod() throws InterruptedException {
+
+        latch.await();
+        retrieveValue();
+        latch.countDown();
+
+    }
+
+    void retrieveValue() {
+        System.out.println("Value retrieved.");
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
