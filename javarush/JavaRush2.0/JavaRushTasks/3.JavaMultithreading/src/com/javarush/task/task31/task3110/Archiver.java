3539a8abe314dde54b65d3fe5be4c8b48c2ae8f6
Bogdan Popov <popovbodya@gmail.com>
2017-03-05 19:21:05 +0300

    some solutions of 31 lvl big task added

--- javarush/JavaRush2.0/JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task31/task3110/Archiver.java
+++ javarush/JavaRush2.0/JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task31/task3110/Archiver.java
@@ -1,32 +1,37 @@
 package com.javarush.task.task31.task3110;
 
-/*
-5. Реализуй метод execute() в классе ExitCommand, он должен выводить “До встречи!” с помощью
-метода из класса ConsoleHelper
-6. В самом конце метода main в класса Archiver добавь код, который создает объект типа ExitCommand и вызывает у него метод execute()
-7. Попробуй, как это все работает
-Обрати внимание, что все файлы проекта должны быть в кодировке UTF-8. Кодировку в IntelliJ IDEA можно задать через пункты меню Settings -> Editor -> File Encodings.
-Проверь, что все три поля отвечающие за кодировку выставлены в UTF-8.
- */
+import com.javarush.task.task31.task3110.exception.WrongZipFileException;
 
-import com.javarush.task.task31.task3110.command.ExitCommand;
-
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.nio.file.Paths;
 
 public class Archiver {
+    public static void main(String[] args) throws IOException {
 
-    public static void main(String[] args) throws Exception {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        System.out.println("Введите полный путь до архива");
-        String fileName = reader.readLine();
-        ZipFileManager zipFileManager = new ZipFileManager(Paths.get(fileName));
-        System.out.println("Введите полный путь до файла для архивации");
-        zipFileManager.createZip(Paths.get(reader.readLine()));
+        Operation operation = null;
+        do {
+            try {
+                operation = askOperation();
+                CommandExecutor.execute(operation);
+            } catch (WrongZipFileException e) {
+                ConsoleHelper.writeMessage("Вы не выбрали файл архива или выбрали неверный файл.");
+            } catch (Exception e) {
+                ConsoleHelper.writeMessage("Произошла ошибка. Проверьте введенные данные.");
+            }
 
-        ExitCommand exitCommand = new ExitCommand();
-        exitCommand.execute();
+        } while (operation != Operation.EXIT);
+    }
+
+
+    public static Operation askOperation() throws IOException {
+        ConsoleHelper.writeMessage("");
+        ConsoleHelper.writeMessage("Выберите операцию:");
+        ConsoleHelper.writeMessage(String.format("\t %d - упаковать файлы в архив", Operation.CREATE.ordinal()));
+        ConsoleHelper.writeMessage(String.format("\t %d - добавить файл в архив", Operation.ADD.ordinal()));
+        ConsoleHelper.writeMessage(String.format("\t %d - удалить файл из архива", Operation.REMOVE.ordinal()));
+        ConsoleHelper.writeMessage(String.format("\t %d - распаковать архив", Operation.EXTRACT.ordinal()));
+        ConsoleHelper.writeMessage(String.format("\t %d - просмотреть содержимое архива", Operation.CONTENT.ordinal()));
+        ConsoleHelper.writeMessage(String.format("\t %d - выход", Operation.EXIT.ordinal()));
+
+        return Operation.values()[ConsoleHelper.readInt()];
     }
 }
