b6230e8e51202e5a2b710e729627efdf3147a677
Bogdan Popov <popovbodya@gmail.com>
2017-03-07 22:13:43 +0300

    some 28 lvl solutions added

--- javarush/JavaRush2.0/JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task28/task2812/Solution.java
+++ javarush/JavaRush2.0/JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task28/task2812/Solution.java
@@ -1,14 +1,13 @@
 package com.javarush.task.task28.task2812;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 /* 
 ShutdownNow!
-Разберись в чем отличие методов shutdown и shutdownNow. Выведи список всех незавершенных задач на экран.
-
-Должно получиться что-то вроде:
 pool-1-thread-1, localId=1
 pool-1-thread-2, localId=2
 pool-1-thread-3, localId=3
@@ -24,16 +23,23 @@ java.util.concurrent.FutureTask@6576fe71 was not completed
 public class Solution {
     public static void main(String[] args) throws InterruptedException {
         ExecutorService executor = Executors.newFixedThreadPool(5);
+        List<Future> futures = new ArrayList<>();
+
         for (int i = 1; i <= 10; i++) {
             final int localId = i;
-            executor.submit(new Runnable() {
+            Future<?> task = executor.submit(new Runnable() {
                 public void run() {
                     doExpensiveOperation(localId);
                 }
             });
+            futures.add(task);
         }
 
-        executor.shutdownNow();
+        final List<Runnable> rejected = executor.shutdownNow();
+        for (Runnable runnable: rejected) {
+            System.out.println(runnable + " was not completed");
+        }
+
     }
 
     private static void doExpensiveOperation(int localId) {
