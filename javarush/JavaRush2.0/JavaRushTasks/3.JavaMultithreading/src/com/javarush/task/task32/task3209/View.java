5f5b60dbde59c1bfa489efbb252423f9eab079eb
Bogdan Popov <popovbodya@gmail.com>
2017-03-09 20:23:40 +0300

    big task solutions added

--- javarush/JavaRush2.0/JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task32/task3209/View.java
+++ javarush/JavaRush2.0/JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task32/task3209/View.java
@@ -2,22 +2,31 @@ package com.javarush.task.task32.task3209;
 
 import com.javarush.task.task32.task3209.listeners.FrameListener;
 import com.javarush.task.task32.task3209.listeners.TabbedPaneChangeListener;
+import com.javarush.task.task32.task3209.listeners.UndoListener;
 
 import javax.swing.*;
+import javax.swing.undo.CannotRedoException;
+import javax.swing.undo.CannotUndoException;
+import javax.swing.undo.UndoManager;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 /*
-HTML Editor (9)
-9.1. Реализуй метод initMenuBar(). Он должен:
-9.1.1. Создавать новый объект типа JMenuBar. Это и будет наша панель меню.
-9.1.2. С помощью MenuHelper инициализировать меню в следующем порядке: Файл, Редактировать, Стиль, Выравнивание, Цвет, Шрифт и Помощь.
-9.1.3. Добавлять в верхнюю часть панели контента текущего фрейма нашу панель меню, аналогично тому, как это мы делали с панелью вкладок.
-9.2. Добавь конструктор класса View. Он должен устанавливать внешний вид и поведение (look and feel) нашего приложения такими же, как это определено в системе.
-Конструктор не должен кидать исключений, только логировать их с помощью ExceptionHandler.
-
-Подсказа: для реализации задания используй класс UIManager.
+11.1. Добавь в представление поле UndoManager undoManager. Разберись для чего используется этот класс. Проинициализируй поле класса новым объектом.
+11.2. Добавь класс UndoListener реализующий интерфейс UndoableEditListener в пакет listeners. Этот класс будет следить за правками, которые можно отменить или вернуть.
+11.3. Добавь в класс UndoListener:
+11.3.1. Поле UndoManager undoManager.
+11.3.2. Конструктор, который принимает UndoManager и инициализирует поле класса.
+11.3.3. Метод undoableEditHappened(UndoableEditEvent e). Он должен из переданного события получать правку и добавлять ее в undoManager.
+11.4. Добавь в представление поле UndoListener undoListener, проинициализируй его используя undoManager.
+11.5. Добавь в представление методы:
+11.5.1. void undo() — отменяет последнее действие. Реализуй его используя undoManager.
+Метод не должен кидать исключений, логируй их.
+11.5.2. void redo() — возвращает ранее отмененное действие. Реализуй его по аналогии с предыдущим пунктом.
+11.5.3. Реализуй методы boolean canUndo() и boolean canRedo() используя undoManager.
+11.5.4. Реализуй геттер для undoListener.
+11.5.5. Добавь и реализуй метод void resetUndo(), который должен сбрасывать все правки в менеджере undoManager.
  */
 
 
@@ -27,6 +36,8 @@ public class View extends JFrame implements ActionListener {
     private JTabbedPane tabbedPane = new JTabbedPane(); // это будет панель с двумя вкладками.
     private JTextPane htmlTextPane = new JTextPane(); // это будет компонент для визуального редактирования html. Он будет размещен на первой вкладке.
     private JEditorPane plainTextPane = new JEditorPane(); // это будет компонент для редактирования html в виде текста, он будет отображать код html (теги и их содержимое).
+    private UndoManager undoManager = new UndoManager();
+    private UndoListener undoListener = new UndoListener(undoManager);
 
     public View() {
         try {
@@ -40,6 +51,27 @@ public class View extends JFrame implements ActionListener {
     public void actionPerformed(ActionEvent e) {
 
     }
+
+    public void resetUndo() {
+        undoManager.discardAllEdits();
+    }
+
+    public void undo() {
+        try {
+            undoManager.undo();
+        } catch (CannotUndoException e) {
+            ExceptionHandler.log(e);
+        }
+    }
+
+    public void redo() {
+        try {
+            undoManager.redo();
+        } catch (CannotRedoException e) {
+            ExceptionHandler.log(e);
+        }
+    }
+
 
     public void init() {
         initGui();
@@ -104,5 +136,18 @@ public class View extends JFrame implements ActionListener {
 
     public void selectedTabChanged() {
 
+    }
+
+    public boolean canUndo() {
+
+        return undoManager.canUndo();
+    }
+
+    public boolean canRedo() {
+        return undoManager.canRedo();
+    }
+
+    public UndoListener getUndoListener() {
+        return undoListener;
     }
 }
