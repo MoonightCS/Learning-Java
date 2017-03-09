1ace9c005967553f3dba98836a34a8f6c4f8ed23
Bogdan Popov <popovbodya@gmail.com>
2017-03-09 21:03:32 +0300

    big task solutions added

--- javarush/JavaRush2.0/JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task32/task3209/Controller.java
+++ javarush/JavaRush2.0/JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task32/task3209/Controller.java
@@ -1,19 +1,21 @@
 package com.javarush.task.task32.task3209;
 
-/*
-2.1. Добавь в контроллер и представление по методу init(), пока без реализаций. Они будут отвечать за инициализацию контроллера и представления.
-2.2. Теперь напишем в классе Controller метод main(). Он должен:
-2.2.1. Создавать объект представления.
-2.2.2. Создавать контроллер, используя представление.
-2.2.3. Устанавливать у представления контроллер.
-2.2.4. Инициализировать представление.
-2.2.5. Инициализировать контроллер. Контроллер должен инициализироваться после представления.
-2.3. Добавь в контроллер метод exit(), он должен вызывать статический метод exit у класса System.
-2.4. Добавь в представление метод exit(), он должен вызывать exit() у контроллера.
- */
 
 import javax.swing.text.html.HTMLDocument;
+import javax.swing.text.html.HTMLEditorKit;
 import java.io.File;
+
+/*
+Добавь в контроллер метод resetDocument(), который будет сбрасывать текущий документ. Он должен:
+15.1. Удалять у текущего документа document слушателя правок которые можно отменить/вернуть (найди подходящий для этого метод, унаследованный от AbstractDocument). Слушателя нужно запросить у представления (метод getUndoListener()).
+Не забудь проверить, что текущий документ существует (не null).
+15.2. Создавать новый документ по умолчанию и присваивать его полю document.
+
+Подсказка: воспользуйся подходящим методом класса HTMLEditorKit.
+
+15.3. Добавлять новому документу слушателя правок.
+15.4. Вызывать у представления метод update().
+ */
 
 public class Controller {
 
@@ -29,6 +31,22 @@ public class Controller {
 
     }
 
+    /*
+    Метод resetDocument() должен удалять у текущего документа document слушателя правок через метод removeUndoableEditListener().
+     */
+
+    public void resetDocument() {
+
+        if (this.document != null && view != null) {
+            document.removeUndoableEditListener(view.getUndoListener());
+        }
+        document = (HTMLDocument) new HTMLEditorKit().createDefaultDocument();
+        if (document.getUndoableEditListeners() == null) {
+            this.document.addUndoableEditListener(view.getUndoListener());
+        }
+        view.update();
+    }
+
     public void exit() {
         System.exit(0);
     }
@@ -41,4 +59,9 @@ public class Controller {
         controller.init();
         //view.exit();
     }
+
+    public HTMLDocument getDocument() {
+        return document;
+    }
+
 }
--- javarush/JavaRush2.0/JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task32/task3209/View.java
+++ javarush/JavaRush2.0/JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task32/task3209/View.java
@@ -13,15 +13,12 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 /*
-Реализуем класс TextEditMenuListener в пакет listeners. Этот класс будет работать аналогично классу UndoMenuListener только для других пунктов меню. Пункты меню, отвечающие за стиль, шрифт,
-цвет и т.д. должны быть доступны только тогда, когда в нашем редакторе выбрана первая вкладка.
-13.1. Добавь в представление метод boolean isHtmlTabSelected(). Он должен возвращать true, если выбрана вкладка, отображающая html в панели вкладок (подсказка: ее индекс 0).
-13.2. Добавь в класс TextEditMenuListener поле View, проинициализируй его в конструкторе класса.
-13.3. В классе TextEditMenuListener переопредели метод menuSelected(MenuEvent menuEvent). Он должен:
-13.3.1. Из переданного параметра получать объект, над которым было совершено действие. В нашем случае это будет объект с типом JMenu.
-13.3.2. У полученного меню получать список компонентов (пунктов меню).
-13.3.3. Для каждого пункта меню вызывать метод setEnabled, передав в качестве параметра результат вызова метода isHtmlTabSelected() из представления. Запусти приложение и убедись, что пункты меню стиль, выравнивание,
-цвет и шрифт доступны только, когда активна закладка HTML и не доступны для закладки Текст.
+14.1. Добавь в класс представления метод selectHtmlTab(). Он должен:
+14.1.1. Выбирать html вкладку (переключаться на нее).
+14.1.2. Сбрасывать все правки с помощью метода, который ты реализовал ранее.
+14.2. Добавь в класс контроллера геттер для модели, в нашем случае это поле document.
+14.3. Добавь в представление метод update(), который должен получать документ у контроллера и устанавливать его в панель редактирования htmlTextPane.
+14.4. Добавь в представление метод showAbout(), который должен показывать диалоговое окно с информацией о программе. Информацию придумай сам, а вот тип сообщения должен быть JOptionPane.INFORMATION_MESSAGE.
  */
 
 
@@ -45,6 +42,19 @@ public class View extends JFrame implements ActionListener {
     @Override
     public void actionPerformed(ActionEvent e) {
 
+    }
+
+    public void showAbout() {
+        JOptionPane.showMessageDialog(this, "Bodya's HTML Editor", "About", JOptionPane.INFORMATION_MESSAGE);
+    }
+
+    public void update() {
+        htmlTextPane.setDocument(controller.getDocument());
+    }
+
+    public void selectHtmlTab() {
+        tabbedPane.setSelectedIndex(0);
+        resetUndo();
     }
 
     public boolean isHtmlTabSelected() {
