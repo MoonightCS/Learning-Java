a2bfd8498dc683bed79de37e5a3dbe064c2a0dfb
Bogdan Popov <popovbodya@gmail.com>
2017-03-17 16:54:09 +0300

    some 30 lvl solutions added

---
+++ javarush/JavaRush2.0/JavaRushTasks/3.JavaMultithreading/src/com/javarush/task/task30/task3004/BinaryRepresentationTask.java
@@ -0,0 +1,34 @@
+package com.javarush.task.task30.task3004;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.RecursiveTask;
+
+/*
+2. Реализуй логику метода compute — число должно переводиться в двоичное представление.
+3. Используй методы fork и join.
+4. Пример функциональной реализации — метод binaryRepresentationMethod.
+
+ */
+
+public class BinaryRepresentationTask extends RecursiveTask<String> {
+
+    private int x;
+
+    public BinaryRepresentationTask(int x) {
+        this.x = x;
+    }
+
+    @Override
+    protected String compute() {
+        int a = x % 2;
+        int b = x / 2;
+        String result = String.valueOf(a);
+        if (b > 0) {
+            BinaryRepresentationTask task = new BinaryRepresentationTask(b);
+            task.fork();
+            return task.join() + result;
+        }
+        return result;
+    }
+}
