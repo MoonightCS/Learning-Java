58416f37afdd9e97516dd709bf33f462c35366f4
Bogdan Popov <popovbodya@gmail.com>
2017-05-08 22:30:57 +0300

    updated current work

---
+++ stepik/web_services/current_work/src/main/java/ru/popov/bodya/executor/Executor.java
@@ -0,0 +1,34 @@
+package ru.popov.bodya.executor;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class Executor {
+
+    public static <T> T execQuery(Connection connection, String query, ResultHandler<T> handler) throws SQLException {
+
+        T value = null;
+        try (Statement statement = connection.createStatement()) {
+            statement.execute(query);
+            ResultSet resultSet = statement.getResultSet();
+            value = handler.handle(resultSet);
+        }
+        return value;
+    }
+
+    public static int execUpdate(Connection connection, String update) {
+        int updated = 0;
+        try (Statement stmt = connection.createStatement()) {
+            stmt.execute(update);
+            updated = stmt.getUpdateCount();
+            stmt.close();
+            return updated;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return updated;
+    }
+
+}
