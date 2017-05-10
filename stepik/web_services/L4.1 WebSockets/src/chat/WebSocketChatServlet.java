f3039d2665bd26644c00282cf0ecd8a9b2c2ff76
Bogdan Popov <popovbodya@gmail.com>
2017-05-10 19:29:42 +0300

    web sockets example added

---
+++ stepik/web_services/L4.1 WebSockets/src/chat/WebSocketChatServlet.java
@@ -0,0 +1,33 @@
+package chat;
+
+import org.eclipse.jetty.websocket.servlet.*;
+
+import javax.servlet.annotation.WebServlet;
+
+/**
+ * @author v.chibrikov
+ *         <p>
+ *         Пример кода для курса на https://stepic.org/
+ *         <p>
+ *         Описание курса и лицензия: https://github.com/vitaly-chibrikov/stepic_java_webserver
+ */
+@WebServlet(name = "WebSocketChatServlet", urlPatterns = {"/chat"})
+public class WebSocketChatServlet extends WebSocketServlet {
+    private final static int LOGOUT_TIME = 10 * 60 * 1000;
+    private final ChatService chatService;
+
+    public WebSocketChatServlet() {
+        this.chatService = new ChatService();
+    }
+
+    @Override
+    public void configure(WebSocketServletFactory factory) {
+        factory.getPolicy().setIdleTimeout(LOGOUT_TIME);
+        factory.setCreator(new WebSocketCreator() {
+            @Override
+            public Object createWebSocket(ServletUpgradeRequest req, ServletUpgradeResponse resp) {
+                return new ChatWebSocket(chatService);
+            }
+        });
+    }
+}
